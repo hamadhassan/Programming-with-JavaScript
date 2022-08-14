5baa934407ed4a6683e80d6c71606ecf8b706460
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-08-14 17:08:02 +0500

    functions and arrays

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 console.log("hello world")
-console.log("This")
+function addName(mesg){
+    console.log("Hello",mesg);
+}function sum(a,b){
+    console.log(a+b);
+}
+sum(20,30);
+addName("Ali");
+var train=["wheat","salt","maaz","cotton"];
+console.log(train[3])
