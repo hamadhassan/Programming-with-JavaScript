e95f4efe8ae2a6cc491a9a76d320f9d8dfe885e0
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-08-15 10:33:42 +0500

    arrays are objects

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ var table ={
 console.log(table);
 table.wood="special";
 console.log(table);
+//Arrays are objects
+var fruits=[];
+fruits.push("apple");
+fruits.push('banana');
+console.log(fruits)
+fruits.pop();
+console.log(fruits)
+
+var array=[];
+function arrayBuilder(one,two,three){
+    array.push(one);
+    array.push(two);
+    array.push(three);
+}
+arrayBuilder(1,2,3);
+arrayBuilder(4,5,6);
+console.log(array);
+
